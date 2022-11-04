37b923c326d57bfe622c52e2372e07fec492aa6a
Albert <jonas.albert.oktatas@gmail.com>
2022-11-04 19:03:35 +0100

    Első teszt ellenőrző lépés nélkül

--- calculator.py
+++ calculator.py
@@ -1,10 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+import time
+
 driver = webdriver.Chrome()
 
 driver.get("https://www.desmos.com/scientific")
 driver.maximize_window()
 
+time.sleep(2)
 
-driver.find_element(By.xpath, value="lastName").send_keys("bo2023")
+
+def test(a,b,muvelet):
+    driver.find_element(By.CSS_SELECTOR, "span[aria-label='{}']".format(a)).click()
+    time.sleep(2)
+    driver.find_element(By.CSS_SELECTOR, "span[aria-label='{}']".format(muvelet)).click()
+    time.sleep(2)
+    driver.find_element(By.CSS_SELECTOR, "span[aria-label='{}']".format(b)).click()
+
+    #Ide kell az ellenőrző lépés
+
+test(1,2,"Plus")
