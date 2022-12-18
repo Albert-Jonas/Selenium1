332f77feff493e1145db3263c861478c218038fb
bozso69 <bozsoky.zoltan@bozsofalvi.hu>
2022-12-18 23:47:20 +0100

    Kommentezés

--- calculator.py
+++ calculator.py
@@ -8,13 +8,13 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--lang= hu")
 
 
-
+# A browser indítása
 def startBrowser():
     driver = webdriver.Chrome()
     driver.get("https://www.desmos.com/scientific")
     return driver
 
-
+# Gombok műveleti jel nyomogatása a felületen
 def test(a,b,muvelet):
     for i in str(a):
         driver.find_element(By.CSS_SELECTOR, "span[aria-label='{}']".format(i)).click()
@@ -24,12 +24,136 @@ def test(a,b,muvelet):
     for i in str(b):
         driver.find_element(By.CSS_SELECTOR, "span[aria-label='{}']".format(i)).click()
 
+# Eredmény beolvasása a felületről
 def eredmeny():
     vissza = driver.find_elements(By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div/div[1]/div[2]/div[1]/div[5]/div[2]/div[1]")
     strErtek = ""
     for Item in vissza:
         strErtek += Item.text
     return strErtek[1:]
+
+def biralat(szam3, eredmenystr, i):
+    try:
+        assert str(szam3)  == eredmenystr, "Hibás"
+    except AssertionError as e:
+        ws['F' + str(i)] = "Fail"
+        ws['G' + str(i)] = str(e)
+    else:
+        ws['F' + str(i)] = "Pass"
+
+# Összeadás művelet
+def osszeadas_pozitiv_szamokkal(szam1, szam2, szam3, i):
+    time.sleep(2)
+    test(szam1, szam2, "Összeadás")
+    time.sleep(2)
+    eredmenystr = eredmeny()
+    biralat(szam3, eredmenystr,i)
+
+
+# Kivonás művelet
+def kivonas_pozitiv_szamokkal(szam1, szam2, szam3, i):
+    time.sleep(2)
+    test(szam1, szam2, "Kivonás")
+    time.sleep(2)
+    eredmenystr = eredmeny()
+    biralat(szam3, eredmenystr, i)
+
+
+# Szorzás művelet
+def szorzas_pozitiv_szamokkal(szam1, szam2, szam3, i):
+    time.sleep(2)
+    test(szam1, szam2, "Szorzás")
+    time.sleep(2)
+    eredmenystr = eredmeny()
+    biralat(szam3,eredmenystr, i)
+
+# Összeadás művelet
+def osztas_pozitiv_szamokkal(szam1, szam2, szam3, i):
+    time.sleep(2)
+    test(szam1, szam2, "Osztás")
+    time.sleep(2)
+    eredmenystr = eredmeny()
+    biralat(szam3, eredmenystr, i)
+
+# Kinulázza a számológép beviteli mezőt
+def clear():
+    driver.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div/div[3]/div[1]/div/div[7]").click()
+
+
+#  Excel site, cellákat alaphelyzetbe (üresre) állítja
+def excelReset(ws,wb):
+    i=3
+    while (ws['B' + str(i)].value):
+        ws['F' + str(i)] = " "
+        ws['G' + str(i)] = " "
+        i = i + 1
+
+# Start
+driver = startBrowser()
+# tabla , site
+wb = load_workbook('excel.xlsx')
+ws = wb["Összeadás"]
+
+# müveleti jelzö (1-4)
+muvelet = 1
+# Művelet választó - Site hozzárendelés
+while(muvelet <=4):
+    # print(muvelet)
+    match muvelet:
+        case 1:
+            ws = wb["Összeadás"]
+            excelReset(ws,wb)
+        case 2:
+            ws = wb["Kivonás"]
+            excelReset(ws, wb)
+        case 3:
+            ws = wb["Szorzás"]
+            excelReset(ws, wb)
+        case 4:
+            ws = wb["Osztás"]
+            excelReset(ws, wb)
+
+    i = 3
+    # Megfelelő számolás indítésa
+    while(ws['B'+str(i)].value):
+        match muvelet:
+            case 1:
+                osszeadas_pozitiv_szamokkal(ws['B'+str(i)].value, ws['C'+str(i)].value, ws['D'+str(i)].value, i)
+            case 2:
+                kivonas_pozitiv_szamokkal(ws['B'+str(i)].value, ws['C'+str(i)].value, ws['D'+str(i)].value, i)
+            case 3:
+                szorzas_pozitiv_szamokkal(ws['B'+str(i)].value, ws['C'+str(i)].value, ws['D'+str(i)].value, i)
+            case 4:
+                osztas_pozitiv_szamokkal(ws['B'+str(i)].value, ws['C'+str(i)].value, ws['D'+str(i)].value, i)
+        # számláló növelése
+        i = i + 1
+        # Beviteli mező tőrlő hívása
+        clear()
+    # Müvelet kiválasztó érték növelése
+    muvelet = muvelet+1
+# Excel tábla mentése
+wb.save('excel.xlsx')
+# Oldal bezárása
+driver.close()
+# driver.close()
+# *************************** Raktár
+
+#osszeadas_pozitiv_szamokkal(10,20,"30")
+#clear()
+#kivonas_pozitiv_szamokkal(20,10,"10")
+#clear()
+#szorzas_pozitiv_szamokkal(10,20,"200")
+#clear()
+#osztas_pozitiv_szamokkal(10,20,"0.5")
+#clear()
+
+# try:
+    #     assert str(szam3) == eredmenystr, "Hibás"
+    # except AssertionError as e:
+    #     ws['F' + str(i)] = "Fail"
+    #     ws['G' + str(i)] = str(e)
+    # else:
+    #     ws['F' + str(i)] = "Pass"
 
 # driver.maximize_window()
 # test(random.randint(0,9999999),random.randint(0,999999),"Kivonás")
@@ -51,116 +175,7 @@ def eredmeny():
 # osszeadas_pozitiv_szamokkal(random.randint(1,9999999),random.randint(1,999999))
 # osszeadas_pozitiv_szamokkal(10,20)
 # assert "30" == osszeadas_pozitiv_szamokkal(10,20,30)
-def biralat(szam3, eredmenystr, i):
-    try:
-        assert str(szam3)  == eredmenystr, "Hibás"
-    except AssertionError as e:
-        ws['F' + str(i)] = "Fail"
-        ws['G' + str(i)] = str(e)
-    else:
-        ws['F' + str(i)] = "Pass"
 
+# Eldönti, hogy a művelet a várt eredményt hozza-e
 
-def osszeadas_pozitiv_szamokkal(szam1, szam2, szam3, i):
-    time.sleep(2)
-    test(szam1, szam2, "Összeadás")
-    time.sleep(2)
-    eredmenystr = eredmeny()
-    biralat(szam3, eredmenystr,i)
-    # try:
-    #     assert str(szam3) == eredmenystr, "Hibás"
-    # except AssertionError as e:
-    #     ws['F' + str(i)] = "Fail"
-    #     ws['G' + str(i)] = str(e)
-    # else:
-    #     ws['F' + str(i)] = "Pass"
-
-
-
-def kivonas_pozitiv_szamokkal(szam1, szam2, szam3, i):
-    time.sleep(2)
-    test(szam1, szam2, "Kivonás")
-    time.sleep(2)
-    eredmenystr = eredmeny()
-    biralat(szam3, eredmenystr, i)
-
-    # assert szam3 == eredmenystr, "Hibás"
-def szorzas_pozitiv_szamokkal(szam1, szam2, szam3, i):
-    time.sleep(2)
-    test(szam1, szam2, "Szorzás")
-    time.sleep(2)
-    eredmenystr = eredmeny()
-    biralat(szam3,eredmenystr, i)
-    # assert szam3 == eredmenystr, "Hibás"
-def osztas_pozitiv_szamokkal(szam1, szam2, szam3, i):
-    time.sleep(2)
-    test(szam1, szam2, "Osztás")
-    time.sleep(2)
-    eredmenystr = eredmeny()
-    # assert szam3 == eredmenystr, "Hibás"
-    biralat(szam3, eredmenystr, i)
-
-def clear():
-    driver.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div/div[3]/div[1]/div/div[7]").click()
-
-
-
-
-driver = startBrowser()
-#osszeadas_pozitiv_szamokkal(10,20,"30")
-#clear()
-#kivonas_pozitiv_szamokkal(20,10,"10")
-#clear()
-#szorzas_pozitiv_szamokkal(10,20,"200")
-#clear()
-#osztas_pozitiv_szamokkal(10,20,"0.5")
-#clear()
-
-def excelReset(ws,wb):
-    i=3
-    while (ws['B' + str(i)].value):
-        ws['F' + str(i)] = " "
-        ws['G' + str(i)] = " "
-        i = i + 1
-
-
-
-# excelReset()
-
-wb = load_workbook('excel.xlsx')
-ws = wb["Összeadás"]
-muvelet = 1
-while(muvelet <=4):
-    print(muvelet)
-    match muvelet:
-        case 1:
-            ws = wb["Összeadás"]
-            excelReset(ws,wb)
-        case 2:
-            ws = wb["Kivonás"]
-            excelReset(ws, wb)
-        case 3:
-            ws = wb["Szorzás"]
-            excelReset(ws, wb)
-        case 4:
-            ws = wb["Osztás"]
-            excelReset(ws, wb)
-    print(ws)
-    i = 3
-    while(ws['B'+str(i)].value):
-        match muvelet:
-            case 1:
-                osszeadas_pozitiv_szamokkal(ws['B'+str(i)].value, ws['C'+str(i)].value, ws['D'+str(i)].value, i)
-            case 2:
-                kivonas_pozitiv_szamokkal(ws['B'+str(i)].value, ws['C'+str(i)].value, ws['D'+str(i)].value, i)
-            case 3:
-                szorzas_pozitiv_szamokkal(ws['B'+str(i)].value, ws['C'+str(i)].value, ws['D'+str(i)].value, i)
-            case 4:
-                osztas_pozitiv_szamokkal(ws['B'+str(i)].value, ws['C'+str(i)].value, ws['D'+str(i)].value, i)
-        i = i + 1
-        clear()
-    muvelet = muvelet+1
-
-wb.save('excel.xlsx')
-driver.close()
-# driver.close()
+# assert szam3 == eredmenystr, "Hibás"
