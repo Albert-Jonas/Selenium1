326127261e00178a18dbadcb58a35d66561cc56c
bozso69 <bozsoky.zoltan@bozsofalvi.hu>
2023-01-01 16:00:52 +0100

    password.py - Kódból a rem-ek kiszedése. - elemszam2 - létrehozása(ellenőrzi, hogy csak "INin" betűket lehessen beírni)

--- password.py
+++ password.py
@@ -1,42 +1,65 @@
 from random import random, randint
 
+def jelkeszlet(jelek):
+    print(jelek)
+def kerdesFG(szoveg):
+    return input(szoveg)
+
 def kisBetuk():
     return ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 def nagyBetuk():
-    kerdes = input("Nagybetűt tartalmazzon: (i/n) ")
+    kerdes = kerdesFG("Nagybetűt tartalmazzon: (i/n) ")
     if kerdes == "i":
         return ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     else:
         return []
+
+def nagyBetuk2():
+    jelkeszlet(jelek)
+    kerdes = "a"
+    while(kerdes not in "inIN"):
+        kerdes = kerdesFG("Nagybetűt tartalmazzon: (i/n) ")
+    if kerdes == "i":
+        return ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+                "U", "V", "W", "X", "Y", "Z"]
+    else: return []
+
 def szamok():
-    kerdes = input("Számokat tartalmazzon: (i/n) ")
+    kerdes = kerdesFG("Számokat tartalmazzon: (i/n) ")
     if kerdes == "i":
         return ["0","1","2","3","4","5","6","7","8","9"]
     else:
         return []
 def kulunlegesJelek():
-    kerdes = input("Különleges jeleket tartalmazzon: (i/n) ")
+    kerdes = kerdesFG("Különleges jeleket tartalmazzon: (i/n) ")
     if kerdes == "i":
         return ["^","&","@","#","$","-","_","!","$","%","~","?","=",">","<",".","*",";","+",":"]
     else:
         return []
 def elemszam():
     return input("Kérek egy számot 5-50 közőtt: (Jelszó hossza) ")
-
 def osszerak(jelek, hossz):
     vissza = ""
-    for i in range(0,hossz):
+    jelkeszlet(jelek)
+    for i in range(0,int(hossz)):
         elemSzam = randint(0,len(jelek))
         db = jelek[elemSzam]
         # print(db)
         vissza = vissza + db
     return vissza
 
+def elemszam2():
+    vissza = 0
+    while (int(vissza) < 5 or int(vissza) > 50):
+        vissza = input("Kérek egy számot 5-50 közőtt: (Jelszó hossza) ")
+        # print(vissza)
+    return vissza
+
 
 jelek = kisBetuk()
-hossz = elemszam()
-jelek = jelek +  nagyBetuk()
+hossz = elemszam2()
+jelek = jelek +  nagyBetuk2()
 jelek = jelek + szamok()
 jelek = jelek + kulunlegesJelek()
-jelszo= osszerak(jelek, int(hossz))
+jelszo= osszerak(jelek, hossz)
 print(jelszo)
