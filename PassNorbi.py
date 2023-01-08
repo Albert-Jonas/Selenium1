377f0d66b3a2993d3af8e0e105adcf79bb5fc655
Szente123 <nszentesi@gmail.com>
2023-01-08 18:38:25 +0100

    PassNorbi.py - Password generáló 5-50 elemekre

---
+++ PassNorbi.py
@@ -0,0 +1,40 @@
+import random
+
+
+def jelkeszlet(jelek):
+    print(jelek)
+
+def kisBetuk():
+    return  ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+def nagyBetuk():
+    return ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"]
+
+def szamok():
+    return  ["0","1","2","3","4","5","6","7","8","9"]
+
+def kulunlegesJelek():
+    return ["^","&","@","#","$","-","_","!","$","%","~","?","=",">","<",".","*",";","+",":"]
+
+def osszerak(jelek, hossz):
+    vissza = ''
+    for i in range (0,hossz):
+        elemszam = random.randint(0,len(jelek)-1)
+        vissza = vissza + jelek[elemszam]
+    return vissza
+
+def elemszam():
+    vissza = 0
+    while(int(vissza)< 5 or int(vissza)> 50):
+        vissza = input('Kérek egy számot 5 - 50 között : (jelszó hossza) ')
+    return int(vissza)
+
+
+jelek = kisBetuk()
+hossz = elemszam()
+jelek = jelek +  nagyBetuk()
+jelek = jelek + szamok()
+jelek = jelek + kulunlegesJelek()
+jelszo= osszerak(jelek, hossz)
+# jelkeszlet(jelek)
+print('Generált Jelszó : ' + jelszo)
