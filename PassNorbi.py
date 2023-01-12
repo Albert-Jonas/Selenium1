d252d68446aa03a43ac9537e8db05002a98dd069
Szente123 <nszentesi@gmail.com>
2023-01-12 08:58:49 +0100

    PassNorbi.py - Password generáló 5-50 elemre

--- PassNorbi.py
+++ PassNorbi.py
@@ -5,16 +5,32 @@ def jelkeszlet(jelek):
     print(jelek)
 
 def kisBetuk():
-    return  ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+        return  ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
 
 def nagyBetuk():
-    return ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"]
+    kerdes = "a"
+    while(kerdes not in "inIN"):
+        kerdes = input("Nagybetűt tartalmazzon : (i/n)")
+    if kerdes == "i" or "I":
+        return ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"]
+    else:return []
 
 def szamok():
-    return  ["0","1","2","3","4","5","6","7","8","9"]
+    kerdes = "a"
+    while (kerdes not in "inIN"):
+        kerdes = input("Számot tartalmazzon : (i/n)")
+    if kerdes == "i" or "I":
+        return  ["0","1","2","3","4","5","6","7","8","9"]
+    else:return []
 
 def kulunlegesJelek():
-    return ["^","&","@","#","$","-","_","!","$","%","~","?","=",">","<",".","*",";","+",":"]
+    kerdes = "a"
+    while (kerdes not in "inIN"):
+        kerdes = input("Különleges jelet tartalmazzon : (i/n)")
+    if kerdes == "i" or "I":
+        return ["^","&","@","#","$","-","_","!","$","%","~","?","=",">","<",".","*",";","+",":"]
+    else:return []
 
 def osszerak(jelek, hossz):
     vissza = ''
