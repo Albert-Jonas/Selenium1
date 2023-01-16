9ebe4d49f19ace65aa952c235a7c9591784f2eaf
bozso69 <bozsoky.zoltan@bozsofalvi.hu>
2023-01-17 00:55:23 +0100

    koltok.py - Excel muveletk

--- koltok.py
+++ koltok.py
@@ -1,22 +1,72 @@
-from builtins import range, list
-
+from builtins import range, list, str
+from openpyxl.reader.excel import load_workbook
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
+
 
 link = "https://hu.wikipedia.org/wiki/Magyar_k%C3%B6lt%C5%91k,_%C3%ADr%C3%B3k_list%C3%A1ja"
 
 lista = []
+wb = load_workbook("koltok.xlsx")
+ws = wb["költök"]
+excelFejlec= ["Köktő neve", "Születési helye", "Születés időpontja", "Halála ideje"]
 
-driver = webdriver.Chrome()
-driver.get(link)
+# ---------------------------------Excel műveletel
+#Excel tábla kiüritése
+def excelUrites():
+    i = 1
+    while (ws['A' + str(i)].value):
+        ws['A' + str(i)] = " "
+        i = i + 1
+# Excel tábla mentése
+def excelMentes():
+    wb.save("koltok.xlsx")
 
-lista = driver.find_elements(By.PARTIAL_LINK_TEXT, " ")
+# Excel fejlécek beírása a táblázatba
+def excelFejlecBeallitas():
+    ws["A" + "1"] = excelFejlec[0]
+    ws["B" + "1"] = excelFejlec[1]
+    ws["C" + "1"] = excelFejlec[2]
+    ws["D" + "1"] = excelFejlec[3]
 
-for i in range(0, len(lista)):
+def excelListaMentes(lista):
 
-    credIdValue = lista[i].find_elements(By.XPATH("./text"))
-    lista[i] = lista[i].text + " " + credIdValue.text
-    print(lista[i].text.split(" "))
+    # for i in range(0, len(lista)):
+    for i in range(0, 100):
+        print(lista[i].text)
+        ws["A" + str(i+2)] = lista[i].text
 
-driver.close()
+
+# -------------------------------------Web műveletek
+def openBrowser():
+    driver = webdriver.Chrome()
+    driver.get(link)
+    driver.minimize_window()
+    return driver
+
+
+
+
+# Lista létrehozása a wiki oldalról
+# def openBrowswer():
+
+
+def koltoLista(driver):
+    lista = driver.find_elements(By.PARTIAL_LINK_TEXT, " ")
+    return lista
+
+def weblapBezarasa():
+    driver.quit()
+
+
+
+excelUrites()
+excelFejlecBeallitas()
+excelMentes()
+
+
+
+driver = openBrowser()
+lista = koltoLista(driver)
+# excelListaMentes(driver)
+
